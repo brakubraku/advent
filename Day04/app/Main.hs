b8c7d3a4dc1efb6c2d2bd12779009d8681a74edc
branislav <26546655+brakubraku@users.noreply.github.com>
2025-12-20 13:45:17 +0100

    Day04 - part 2

--- Day04/app/Main.hs
+++ Day04/app/Main.hs
@@ -28,24 +28,42 @@ main = do
             '@' -> 1)
     let grid = convertLine <$> gridLinesString
     let result = catMaybes $
-            [if (atGrid (l,r) grid == 1) && (howManyContainersAround grid (l, r) < 4)
+            [if (atGrid grid (l,r) == 1) && (howManyContainersAround grid (l, r) < 4)
                 then Just (l, r) 
                 else Nothing 
             | l <- [0..length grid - 1], r <- [0..length (grid !! 0) - 1]]
-    print $ length result
+    print $ "Part1 - " <> show (length result)
+    let loop grid sum = do
+            let result = catMaybes $
+                    [if (atGrid grid (l,r) == 1) && (howManyContainersAround grid (l, r) < 4)
+                        then Just (l, r) 
+                        else Nothing 
+                    | l <- [0..length grid - 1], r <- [0..length (grid !! 0) - 1]]
+            if not . null $ result 
+            then 
+                let newGrid = foldl (\g (l, r) -> update g (l, r) 0) grid result
+                in loop newGrid (sum + length result)
+            else
+                print $ "Part2 - " ++ show sum
+    loop grid 0
 
 howManyContainersAround :: Grid -> (Int, Int) -> Int 
 howManyContainersAround grid (line, row) = 
     let deltas = [(-1,-1), (-1,0), (-1,1),
                   (0, -1),          (0, 1),
                   (1, -1), (1, 0),  (1, 1)]
-    in sum [atGrid (line + dl, row + dr) grid | (dl, dr) <- deltas]
+    in sum [atGrid grid (line + dl, row + dr) | (dl, dr) <- deltas]
 
-atGrid :: (Int, Int) -> Grid -> Int
-atGrid (l, r) grid = 
+atGrid :: Grid -> (Int, Int) -> Int
+atGrid grid (l, r) = 
             if l < 0 || r < 0 || l >= length grid || r >= length (grid !! 0)
             then 0
             else grid !! l !! r
 
+update grid (l, r) v = 
+    take l grid ++
+    [setAt r v (grid !! l)] ++
+    drop (l + 1) grid
 
-
+setAt :: Int -> a -> [a] -> [a]
+setAt idx val xs = take idx xs ++ [val] ++ drop (idx + 1) xs
