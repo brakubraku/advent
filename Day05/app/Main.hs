f01e72ea0c8c876c756c1d4bfbefe27c586920f9
branislav <26546655+brakubraku@users.noreply.github.com>
2025-12-20 18:03:17 +0100

    Day05 - part 2

--- Day05/app/Main.hs
+++ Day05/app/Main.hs
@@ -5,7 +5,7 @@ import Data.Text (splitOn)
 import qualified Data.Text as T
 import Data.List (sort, sortBy)
 import Data.Ord (comparing)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, isJust)
 
 main :: IO ()
 main = do 
@@ -22,4 +22,29 @@ main = do
         ingInRange i (start, end) = i >= start && i <= end
         isFresh = \i -> if any (==True) $ ingInRange i <$> orderedRanges then Just i else Nothing
         freshIngredients = catMaybes $ isFresh <$> orderedIngredients
-    print $ length freshIngredients
+    print $ "Part1 - " ++ show (length freshIngredients)
+    let mergedRanges = mergeOverLappling orderedRanges
+        sumRanges = sum [end - start + 1 | (start, end) <- mergedRanges]
+    print $ "Part2 - " ++ show sumRanges
+
+mergeOverLappling :: [(Int, Int)] -> [(Int, Int)]
+mergeOverLappling [] = []
+mergeOverLappling (x : xs) =
+  let merged i j =
+        let ij = overlap i j
+            ji = overlap j i
+         in if isJust ij then ij else ji
+      overlap (i1start, i2end) (j1start, j2end) =
+        if j1start <= i2end && j1start >= i1start
+          then Just (i1start, max i2end j2end)
+          else Nothing
+
+      mergeHelper current [] [] _ = [current]
+      mergeHelper current [] (y : ys) False = current : mergeHelper y ys [] False
+      mergeHelper current [] remaining True = mergeHelper current remaining [] False
+      mergeHelper current (y : ys) remaining mergedSomething =
+        case merged current y of
+          Just merged -> mergeHelper merged ys remaining True
+          Nothing -> mergeHelper current ys (y : remaining) mergedSomething
+   
+   in mergeHelper x xs [] False
