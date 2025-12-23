2c614ec0b3974763ec73419f5929876a08e1be70
branislav <26546655+brakubraku@users.noreply.github.com>
2025-12-23 18:44:30 +0100

    Day08 - part 1

---
+++ Day08/app/Main.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+
+import Data.List (nub, partition, sortOn)
+import Data.Text (splitOn)
+import Data.Text qualified as T
+
+main :: IO ()
+main = do
+  lines <- lines <$> readFile "input"
+  let jbxs = (\(x : y : z : []) -> (x, y, z)) <$> (fmap (read @Float . T.unpack) <$> (splitOn "," . T.pack <$> lines))
+  let distances = computeDistance jbxs
+  let thousandShortest = take 1000 $ fst <$> sortOn snd distances
+  let connectedGroups = connect thousandShortest []
+  let largestThree = length <$> (take 3 $ reverse $ sortOn length connectedGroups)
+  print $ "Part1 - " ++ show (foldl (*) 1 largestThree)
+
+computeDistance [] = []
+computeDistance (x : xs) = [((x, y), cartesianDistance3D x y) | y <- xs] ++ computeDistance xs
+
+cartesianDistance3D :: (Floating a) => (a, a, a) -> (a, a, a) -> a
+cartesianDistance3D (x1, y1, z1) (x2, y2, z2) =
+  sqrt ((x2 - x1) ^ 2 + (y2 - y1) ^ 2 + (z2 - z1) ^ 2)
+
+connect [] connectedGroups = connectedGroups
+connect ((x, y) : xs) connectedGroups =
+  let (withXorY, withoutXorY) = partition (\g -> x `elem` g || y `elem` g) connectedGroups
+   in case withXorY of
+        [] -> connect xs ([x, y] : withoutXorY)
+        _ ->
+          let mergedGroup = nub $ x : y : concat withXorY
+           in connect xs (mergedGroup : withoutXorY)
