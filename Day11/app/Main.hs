6db24b0442b09858671c69ae76b71cd6275c058d
branislav <26546655+brakubraku@users.noreply.github.com>
2026-01-11 08:45:48 +0100

    Day 11 - part 2

--- Day11/app/Main.hs
+++ Day11/app/Main.hs
@@ -5,6 +5,9 @@ import qualified Data.Text as T
 import qualified Data.Map as M
 import Data.Maybe
 import Data.Function ((&))
+import System.IO.Unsafe
+
+import Data.IORef 
 
 main :: IO ()
 main = do 
@@ -16,21 +19,40 @@ main = do
            fmap (\(from:to) -> 
                     (fromJust . T.stripSuffix ":" . T.pack $ from, 
                      T.pack <$> to))
+  
   print $ "Part1 - " <> show (length $ findPaths cs "you" "out")
-
-type Connections = M.Map T.Text [T.Text]
-
-findPaths :: Connections -> T.Text -> T.Text -> [[T.Text]]
+  -- print . length $ findPaths cs "dac" "fft"  -- 0
+  -- print . length $ findPaths cs "fft" "dac"  -- 4605696
+  -- print . length $ findPaths cs "svr" "fft"  -- 5915
+  -- print . length $ findPaths cs "dac" "out"  -- 11567
+  print $ "Part2 - " <> show (4605696 * 5915 * 11567)
+  
+findPaths :: Ord a => M.Map a [a] -> a -> a -> [[a]]
 findPaths cs from to = 
-  let findPaths' :: T.Text -> T.Text -> Maybe [[T.Text]]
-      findPaths' from to = 
-        let nodes = fromMaybe [] $ M.lookup from cs
-          in if null nodes 
-             then Nothing 
-             else if to `elem` nodes 
-                  then Just [[to]]
-                  else Just $
-                     (from:) <$> (concat . catMaybes $ 
-                                    flip findPaths' to <$> nodes)
-  in fromMaybe [] $ findPaths' from to
+   let deadEnd (from,to) = unsafePerformIO $ rememberDeadEnd (from,to) >> pure Nothing
+       findPaths' path from to 
+        | (unsafePerformIO $ isDeadEnd (from,to)) = Nothing
+        | from `elem` path = deadEnd (from,to)
+        | otherwise =
+          let nodes = fromMaybe [] $ M.lookup from cs
+            in if null nodes 
+               then deadEnd (from,to)
+               else if to `elem` nodes 
+                    then Just [[from,to]]
+                    else 
+                      let paths = (concat . catMaybes $ 
+                              (\f -> findPaths' (from:path) f to) <$> nodes)
+                      in if null . concat $ paths 
+                         then deadEnd (from,to)
+                         else Just $ (from:) <$> paths
+                         
+  in fromMaybe [] $ findPaths' [] from to
 
+deadEnds = unsafePerformIO $ newIORef M.empty
+
+rememberDeadEnd fromTo = 
+  modifyIORef deadEnds $ (\v -> M.insert fromTo () v)
+
+isDeadEnd fromTo = do 
+   mem <- readIORef deadEnds
+   pure . isJust $ M.lookup fromTo mem
